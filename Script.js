033d53c1c2e0f721c32ba528a7e07481b6041ba6
HawkSWE <83948065+HawkSWE@users.noreply.github.com>
2022-01-20 14:34:50 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
-
+// raden under så skickar jag en förfrågan till hemsidan av att få väder data över ett område i detta fall huddinge
 $.get( "http://api.openweathermap.org/data/2.5/weather?q=Huddinge&appid=c5bf5d99c4ebb07f0a60cbabebec2b95&units=metric", function( data ) {
+
+    // på rad 6 till 12 så sparar jag datan jag får från länken i flera variablar
 
     let namn = data.name
     let temp = Math.floor(data.main.temp)
@@ -8,6 +10,8 @@ $.get( "http://api.openweathermap.org/data/2.5/weather?q=Huddinge&appid=c5bf5d99
     let humidity = Math.floor(data.main.humidity)
     let windspeed = Math.floor(data.wind.speed)
     let imgicon = "http://openweathermap.org/img/wn/"+data.weather[0].icon+".png"
+
+    // På rad 16 till 22 så tar jag datan som jag tidigare sparade i variblarna och lägger in dem på olika h element så användaren kan se datan på hemsidan
 
     $("#stadnamn").html(namn);
     $("#temp").html(temp+" C");

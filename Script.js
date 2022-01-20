90895c91eb7085c1b63227337652e4757dcd5926
HawkSWE <83948065+HawkSWE@users.noreply.github.com>
2022-01-20 12:46:32 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-    $.ajax({
-        url: 'api.openweathermap.org/data/2.5/weather',
-        type: "GET",
-        data: 
-            { 
-            q: "Stockholm" , 
-            appid: 'c5bf5d99c4ebb07f0a60cbabebec2b95', 
-            units: 'metric'
-            }
-      });
-    });
+$.get( "api.openweathermap.org/data/2.5/weather?q=Huddinge&appid=c5bf5d99c4ebb07f0a60cbabebec2b95", function( data ) {
+
+  });

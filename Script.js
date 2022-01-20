fae169c9e6d50a682235e76352fbd93e09d21268
HawkSWE <83948065+HawkSWE@users.noreply.github.com>
2022-01-20 14:03:31 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,19 @@
-$.get( "api.openweathermap.org/data/2.5/weather?q=Huddinge&appid=c5bf5d99c4ebb07f0a60cbabebec2b95", function( data ) {
 
-  });
+$.get( "http://api.openweathermap.org/data/2.5/weather?q=&appid=c5bf5d99c4ebb07f0a60cbabebec2b95&units=metric", function( data ) {
+
+    let namn = data.name
+    let temp = Math.floor(data.main.temp)
+    let tempmin = Math.floor(data.main.temp_min)
+    let tempmax = Math.floor(data.main.temp_max)
+    let humidity = Math.floor(data.main.humidity)
+    let windspeed = Math.floor(data.wind.speed)
+    let imgicon = "http://openweathermap.org/img/wn/"+data.weather[0].icon+".png"
+
+    $("#stadnamn").html(namn);
+    $("#temp").html(temp+" C");
+    $("#mintemp").html(tempmin+" C");
+    $("#maxtemp").html(tempmax+" C");
+    $("#humidity").html(humidity);
+    $("#windspeed").html(windspeed);
+    $("#iconbild").attr('src', imgicon);
+});
